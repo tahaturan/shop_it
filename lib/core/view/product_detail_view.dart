a9f4c13079150f0d2270f9f67a7d54c5e9bdca70
tahaturan <tahaaturann16@gmail.com>
2023-01-13 03:07:56 +0300

    detay sayfasi guncvellendi

--- lib/core/view/product_detail_view.dart
+++ lib/core/view/product_detail_view.dart
@@ -21,60 +21,70 @@ class _ProductDetailViewState extends State<ProductDetailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Padding(
-          padding: ProjectPadding.categoyPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Center(child: ProductImageSize.xxlarge.productImage(widget.product, context)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'USD ${widget.product?.price ?? 0}',
-                    style: TextStyle(
-                      color: ProjectColors.buttonColor.color(),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 21,
-                    ),
-                  ),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border, color: ProjectColors.buttonColor.color())),
-                ],
-              ),
-              Text(widget.product?.title ?? '', style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.black)),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Icon(Icons.star, color: Colors.amber),
-                  Text(
-                    widget.product?.rating?.rate.toString() ?? '',
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.grey, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.1),
-                  Text(
-                    '${widget.product?.rating?.count.toString()} Reviews',
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.grey, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              Text(widget.product?.description ?? '', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.justify),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Add To Card",
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart_outlined),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: ProjectPadding.categoyPadding,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Center(child: ProductImageSize.xxlarge.productImage(widget.product, context)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'USD ${widget.product?.price ?? 0}',
+                      style: TextStyle(
+                        color: ProjectColors.buttonColor.color(),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border, color: ProjectColors.buttonColor.color())),
+                  ],
+                ),
+                Text(widget.product?.title ?? '', style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.black)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(Icons.star, color: Colors.amber),
+                    Text(
+                      widget.product?.rating?.rate.toString() ?? '',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                    Text(
+                      '${widget.product?.rating?.count.toString()} Reviews',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Text(widget.product?.description ?? '', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.justify),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Add To Card",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
